7aa7bac163b84e53aff489c1f3e6750cc3fd382d
Aidan Zebertavage <aidanzeber@gmail.com>
2016-03-13 19:51:02 +0000

    Updated photos controller

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,14 @@
+class PhotosController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    @place = Place.find(params[:place_id])
+    @photo = @place.photos.create(photo_params)
+    redirect_to place_path(@place)
+  end
+
+  private
+
+  def photo_params
+    params.requre(:photo).permit(:caption, :picture)
+  end
+end
