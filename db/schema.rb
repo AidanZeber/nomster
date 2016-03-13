40f534ce134b1874c3d18f3c1064903fe6ea2149
Aidan Zebertavage <aidanzeber@gmail.com>
2016-03-13 20:06:19 +0000

    working on image upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313193822) do
+ActiveRecord::Schema.define(version: 20160313200157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160313193822) do
     t.datetime "updated_at"
     t.text     "caption"
     t.string   "picture"
+    t.integer  "place_id"
   end
 
   create_table "places", force: true do |t|
