092ef0f363a37589e1be616fb3ede8bc6ee2a960
Aidan Zebertavage <aidanzeber@gmail.com>
2016-03-13 18:55:51 +0000

    Created associates in place and user models for photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,4 @@
 class Photo < ActiveRecord::Base
+  belongs_to :user
+  belonts_to :place
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
+  has_many :photos
 	validates :name, :length => {minimum:4}, :presence => true
 	validates :address, :length => {minimum: 4}, :presence => true
 	validates :description, :length => {minimum: 4}, :presence => true
